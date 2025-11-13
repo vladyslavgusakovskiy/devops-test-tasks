1b5d52499dd8094101dd56869a5c879a173055a0
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-11-13 19:10:08 +0200

    add endpoints for probes; create YAML manifests

--- tasks/k8s-monitoring-task-1/app.py
+++ tasks/k8s-monitoring-task-1/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from Kubernetes with Monitoring!"
 
+@app.route("/ready")
+def ready():
+    return "Ready!", 200
+
+@app.route("/health")
+def health():
+    return "Healthy!", 200
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port = int(os.getenv("APP_PORT", 8000)))
