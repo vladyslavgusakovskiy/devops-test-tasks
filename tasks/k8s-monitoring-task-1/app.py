b213d841c06a8e29b2aa4290d6f3eb4ca285e365
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-11-15 19:10:22 +0200

    Modify the app.py to provide metrics compatible with Prometheus; add prometheus_flask_exporter library; add port name to clusterIP.yml

--- tasks/k8s-monitoring-task-1/app.py
+++ tasks/k8s-monitoring-task-1/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 import os
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello():
