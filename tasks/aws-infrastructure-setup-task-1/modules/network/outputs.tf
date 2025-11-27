f5d56fa1e5f15cf4ce6506d35dd0bbcb59a2cfbc
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-11-27 15:47:54 +0200

    Add security group module; remove security_group_id output

--- tasks/aws-infrastructure-setup-task-1/modules/network/outputs.tf
+++ tasks/aws-infrastructure-setup-task-1/modules/network/outputs.tf
@@ -2,8 +2,3 @@ output "subnet_id" {
   description = "The ID of the subnet"
   value       = aws_subnet.main.id
 }
-
-output "security_group_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.main.id
-}
