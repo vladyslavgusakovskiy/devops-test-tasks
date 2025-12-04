6e5b1ae03a8c89a157827e202d15b8b7d5e1f8b7
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-12-04 16:11:31 +0200

    Add ecs and alb modules

--- tasks/aws-infrastructure-setup-task-1/main.tf
+++ tasks/aws-infrastructure-setup-task-1/main.tf
@@ -1,14 +1,44 @@
-module "vpc" {
+module "tm_vpc_module" {
   source     = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
 }
 
-module "network_services" {
+module "tm_network_services_module" {
   source = "./modules/network"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.tm_vpc_module.vpc_id
 }
 
-module "tm_sg" {
+module "tm_sg_module" {
   source = "./modules/securityGroup"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.tm_vpc_module.vpc_id
+}
+
+module "tm_efs_module" {
+  source               = "./modules/efs"
+  tm_efs_sg_id         = module.tm_sg_module.tm_efs_security_group_id
+  tm_subnet_private_id = module.tm_network_services_module.tm_subnet_private_id
+}
+
+module "tm_iam_module" {
+  source  = "./modules/iam"
+  efs_arn = module.tm_efs_module.efs_arn
+}
+
+module "tm_ecs_module" {
+  source                        = "./modules/ecs"
+  tm_efs_id                     = module.tm_efs_module.efs_id
+  tm_subnet_private_id          = module.tm_network_services_module.tm_subnet_private_id
+  tm_security_group_id          = module.tm_sg_module.tm_efs_security_group_id
+  ecs_execution_role_arn        = module.tm_iam_module.ecs_execution_role_arn
+  ecs_task_role_arn             = module.tm_iam_module.ecs_task_role_arn
+  ecs_execution_role_attachment = module.tm_iam_module.ecs_execution_role_attachment_id
+  alb_target_group_arn          = module.tm_alb_module.application_lb_tg_arn
+}
+
+module "tm_alb_module" {
+  source            = "./modules/alb"
+  security_group_id = module.tm_sg_module.tm_efs_security_group_id
+  subnet_pub_1_id   = module.tm_network_services_module.tm_subnet_public_1_id
+  subnet_pub_2_id   = module.tm_network_services_module.tm_subnet_public_2_id
+  vpc_id            = module.tm_vpc_module.vpc_id
 }
